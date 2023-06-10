deb788ca39877606cb52b6bb2f12040321e6fa94
Zaki abdullahi <qable2121@gmail.com>
2023-06-10 19:51:12 +0300

    second

---
+++ me.js
@@ -0,0 +1,75 @@
+/* functions: 
+ labo loo qaybiyaaa 
+
+     1.   function  wax aanan soo celin  waxaana la dhahaa (void function)
+     2    function  wax soo celiya oo execution kiisa dhamaadoo(shaqadaiisa dhamaato)
+
+
+ */
+
+
+/* 
+
+
+
+
+
+*/
+
+// tusaale : function aanan wax soo celin
+
+
+function sayHelo() {
+
+    print("Hellow")
+
+}
+
+
+// function  markii wacano (adeegsano) wuxuuna soo daabacaya    hello
+sayHelo();
+
+
+function sayHelo() {
+
+    return "Hellow";
+
+}
+
+// function kaan markaan  wuxuu noo soo  return gareyndoonaa (celin doona) "hello "
+sayHelo();
+
+// Hint: hadii aan sidaan uga tagno waxba   nooma soo daabacayo .
+
+//  si uuu noogu soo daabaco qiimaha uu noo soo celiyay waan inaan console.log(uga dhexyeerno)
+
+// tusaale
+
+console.log(sayHelo())
+
+// Note:
+
+/* 
+
+  functionka aanan waxba soo celineyn sida void markaad adeegsanaysid 
+  kaliya  waxaad u yeeri function name  sayHelo();
+  
+  functionka xog  soo celineyn sida (return) markaad adeegsanaysid .
+  kaliya  waxaad u yeeri function name  waxaadna ku sheegi console.log(sayHelo()) gudihiisa;
+
+*/
+
+
+// Note:
+
+function sayHelo() {
+
+    return "Hellow";
+    console.log("Hi")
+
+}
+
+/*   return hadad sameysid  waxay  u dhigantaa  functionkaan shaqadiisa waa dhamaatay(end of execution)
+   lama ogola inaad shaqo kale sii qabtid ama so dabacddid
+
+*/
